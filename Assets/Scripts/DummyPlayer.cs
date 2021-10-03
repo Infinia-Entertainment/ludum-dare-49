7f3b9d9d8e5e4960749bca764a74af1cb22941b8
Sirush <delaportemathieu@hotmail.com>
2021-10-03 13:24:18 +0200

    Add bouncy walls unstability modifier

--- Assets/Scripts/DummyPlayer.cs
+++ Assets/Scripts/DummyPlayer.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Spells;
@@ -8,13 +9,21 @@ namespace Wizard
     public class DummyPlayer : MonoBehaviour
     {
         [SerializeField] private GameObject _fireball;
-        
+
+        private Collider2D _collider;
+
+        private void Awake()
+        {
+            _collider = GetComponent<Collider2D>();
+        }
+
         void Update()
         {
             if (Input.GetMouseButtonDown(0))
             {
                 var fb = Instantiate(_fireball, transform.position, Quaternion.identity);
                 fb.GetComponent<SpellBase>().Launch();
+                Physics2D.IgnoreCollision(fb.GetComponent<Collider2D>(), _collider);
             }
         }
     }
--- Assets/Scripts/Spells/Fireball.cs
+++ Assets/Scripts/Spells/Fireball.cs
@@ -11,6 +11,10 @@ namespace Spells
         [SerializeField] private float _impulseStrength = 100f;
         [SerializeField] private VisualEffect _vfx;
 
+        private int _maxBounce = 3;
+        private int _bounceCount = 0;
+        private Vector2 _lastFrameVelocity;
+
         protected override void Awake()
         {
             base.Awake();
@@ -29,11 +33,18 @@ namespace Spells
         {
             _vfx.SetVector3("Velocity", -RigidBody.velocity );
             _vfx.SetVector3("BallPosition", transform.position );
-
+            _lastFrameVelocity = RigidBody.velocity;
         }
 
         private void OnCollisionEnter2D(Collision2D other)
         {
+            if (other.gameObject.CompareTag("Wall") && UnstabilityManager.Instance.BouncyWalls && _bounceCount < _maxBounce)
+            {
+                _bounceCount++;
+                RigidBody.AddForce(Vector2.Reflect(_lastFrameVelocity, other.contacts[0].normal).normalized * _impulseStrength, ForceMode2D.Impulse);
+                return;
+            }
+            
             var damageable = other.gameObject.GetComponent<IDamageable>();
 
             damageable?.OnDamage(Data.Damage);
