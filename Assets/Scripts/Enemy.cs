c5d61f2ecbb8bb6842146cd7f52b29af0542f278
RebornInferno <antonpirogov@mail.ru>
2021-10-04 23:00:29 +0300

    added IDamageable functionality, player can get damaged and die now

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,17 +4,33 @@ namespace Wizard
 {
     public class Enemy : MonoBehaviour, IDamageable
     {
-        private int _health = 1;
+        private int _currentHealth;
+        private int _maxHealth = 1;
+        public int CurrentHealth => _currentHealth;
+        public int MaxHealth => _maxHealth;
+
+
+        private void Start()
+        {
+            _currentHealth = _maxHealth;
+        }
 
         public void OnDamage(int damage)
         {
-            _health -= damage;
-            if (_health <= 0)
+            _currentHealth -= damage;
+            if (_currentHealth <= 0)
             {
-                GameManager.Instance.EnemyCount--;
-                GameManager.Instance.EnemiesKilled++;
-                Destroy(gameObject);
+                OnDeath();
             }
+        }
+
+
+        public void OnDeath()
+        {
+
+            GameManager.Instance.EnemyCount--;
+            GameManager.Instance.EnemiesKilled++;
+            Destroy(gameObject);
         }
     }
 }
--- Assets/Scripts/IDamageable.cs
+++ Assets/Scripts/IDamageable.cs
@@ -2,6 +2,9 @@ namespace Wizard
 {
     public interface IDamageable
     {
+        public int CurrentHealth { get; }
+        public int MaxHealth { get; }
         void OnDamage(int damage);
+        void OnDeath();
     }
 }
